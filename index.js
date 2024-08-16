757a2242cd560da1f2bba3e980a7a9761e3a958a
Volodymyr1233 <105359387+Volodymyr1233@users.noreply.github.com>
2024-08-16 11:36:24 +0300

    Adding score system and replace console.log with alert

--- index.js
+++ index.js
@@ -11,3 +11,56 @@ function checkGuess(playerGuess, correctNumber) {
     return "Your number is too low((";
   }
 }
+
+function getPlayerGuess() {
+  let userGuess;
+
+  while (true) {
+    userGuess = prompt("Please guess a random number");
+    userGuess = parseInt(userGuess, 10);
+
+    if (isNaN(userGuess)) {
+      alert("Invalid input. Please enter a valid number.");
+    } else {
+      return userGuess;
+    }
+  }
+}
+
+function game() {
+  const correctAnswer = "You guessed the number!";
+  const correctNumber = generateRandomNumber();
+  let counter = 0;
+  const maxAttempts = 10;
+  let playerWon = false;
+  let finalScore = 100;
+
+  while (counter < maxAttempts) {
+    let playerGuess = getPlayerGuess();
+    counter++;
+
+    let checkPlayerGuess = checkGuess(playerGuess, correctNumber);
+    alert(checkPlayerGuess);
+
+    if (checkPlayerGuess === correctAnswer) {
+      playerWon = true;
+      break;
+    } else {
+      finalScore -= 10;
+      alert(`-10 points (((. Your score: ${finalScore}`);
+    }
+  }
+
+  if (playerWon) {
+    alert(
+      `Awesome, You won :D with only ${counter} attempts! You get ${finalScore} points! Congratulations!`
+    );
+  } else {
+    alert(
+      `Sorry :( you used all your ${counter} attempts. Try again... You get 0 points (`
+    );
+  }
+}
+
+// start the game
+game();
